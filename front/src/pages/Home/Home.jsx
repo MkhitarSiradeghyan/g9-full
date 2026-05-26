6899b821df276881a0d8b388762bbdb3a69a6205
MkhitarSiradeghyan <mkhitar.siradeghyan@yandex.ru>
2026-05-26 18:12:55 +0400

    Git - Merged branches

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import BrowseByType from '../../components/BrowseByType/BrowseByType';
+import Card from '../../components/Card/Card';
+import Reviews from './../../components/Reviews/Reviews';
+import ReviewCard from '../../components/ReviewCard/ReviewCard';
 
 const Home = () => {
   return (
-    <div>{<BrowseByType />}</div>
+    <div>
+      <BrowseByType/>
+      <Card/>
+      <Reviews/>
+    </div>
   )
 }
 
